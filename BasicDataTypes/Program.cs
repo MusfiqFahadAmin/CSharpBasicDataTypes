31229b679249f939c00a9cacfa08243b1b39a57e
Musfiq Fahad Amin <musfiqfahadamin@gmail.com>
2020-08-22 07:01:17 +0600

    Add project files.

---
+++ BasicDataTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfFruit = "Banana";
+            char startingLetterOfFruitName = 'B';
+            int numberOfFruit = 3;
+            double weightOfFruit = 60.5;
+            decimal priceOfFruit = 0.85m;
+            bool tasteOfFruit = true;
+
+            Console.WriteLine($"Fruit Name: {nameOfFruit}\n" +
+                              $"First Letter of Fruit Name: {startingLetterOfFruitName}\n" +
+                              $"Number of {nameOfFruit}: {numberOfFruit}\n" +
+                              $"Weight of {nameOfFruit}: {weightOfFruit}g\n" +
+                              $"Price of {nameOfFruit}: ${priceOfFruit}\n" +
+                              $"Taste of {nameOfFruit} is Good: {tasteOfFruit}");
+        }
+    }
+}
